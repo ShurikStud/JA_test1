093ba2e6652d6a77482792bd8c008304210dc92b
note-5i\shurik <abashev.alexandr@gmail.com>
2017-10-09 16:54:21 +0300

    переместил MyClass в папку src

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        MyClass myClass = new MyClass();
+
+        myClass.setParam1(12);
+
+        System.out.print(myClass.getParam1());
     }
 }
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class MyClass {
     public int param1;
 
