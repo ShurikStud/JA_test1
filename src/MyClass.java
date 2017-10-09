790bc05fe7425003166cdf4505090e43846baecc
note-5i\shurik <abashev.alexandr@gmail.com>
2017-10-09 16:50:53 +0300

    + MyClass

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public int param1;
+
+    public int getParam1() {
+        return param1;
+    }
+
+    public void setParam1(int param1) {
+        this.param1 = param1;
+    }
+}
